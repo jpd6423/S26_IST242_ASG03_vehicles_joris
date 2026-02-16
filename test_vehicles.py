226e1640bfee6c1bb7e51a72c1c7c57769da2a03
jpd6423 <jpd6423@psu.edu>
2026-02-16 18:13:59 -0500

    Added test file for code
    Tested code for auto_model and manufacturer.

---
+++ test_vehicles.py
@@ -0,0 +1,41 @@
+# This is the unit testing file.
+# Author: Joris Daub
+
+import pytest
+
+from manufacturer import Manufacturer
+from auto_model import AutoModel
+
+class TestManufacturer:
+
+    def test_constructor(self):
+        m = Manufacturer("Ford", "USA")
+        assert m.get_name == "Ford"
+        assert m.get_country == "USA"
+
+    def test_constructor_2(self):
+        m = Manufacturer("Honda","Japan")
+        assert m.get_name == "Honda"
+        assert m.get_country == "Japan"
+
+    def test_str(self):
+        m = Manufacturer("BMW", "Germany")
+        assert str(m) == "(BMW, Germany)"
+
+class TestAutoModel:
+
+    def test_constructor(self):
+        m = AutoModel("F150", True, [2020, 2021, 2022])
+        assert m.get_name == "F150"
+        assert m.get_in_production == True
+        assert m.get_years == [2020, 2021, 2022]
+
+    def test_years_defensive_copy(self):
+        original_list = [2020, 2021]
+        am = AutoModel("F150", True, original_list)
+        original_list.clear()
+        assert am.get_years == [2020,2021]
+
+    def test_str(self):
+        am = AutoModel("F150", True, [2020, 2021, 2022])
+        assert str(am) == "F150 in production = True,  release year: 2020"
