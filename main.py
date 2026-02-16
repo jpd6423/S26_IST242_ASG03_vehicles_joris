04497407f6cb3bcb00e09a20da7434b8567f608d
jpd6423 <jpd6423@psu.edu>
2026-02-16 15:07:37 -0500

    Added manufacturer object print

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from manufacturer import Manufacturer
+
 def main():
+    m = Manufacturer("Ford","USA")
+    print(m.get_name)
+    print(m.get_country)
+    print(m)
     pass
 
 if __name__ == "__main__":
---
+++ manufacturer.py
@@ -0,0 +1,22 @@
+# This represents a vehicle manufacturer
+
+class Manufacturer:
+    # Docstring
+    
+    # Constructor
+    def __init__(self, name: str, country: str):
+        self._name = name
+        self._country = country
+
+    # Properties
+    @property
+    def get_name(self) -> str:
+        return self._name
+    
+    @property
+    def get_country(self) -> str:
+        return self._country
+    
+    # Printing out the manufacturer's objects
+    def __str__(self) -> str:
+        return f"({self._name},{self._country})"
